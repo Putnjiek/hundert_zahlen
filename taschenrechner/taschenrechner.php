508efc8e87b77b744b3336ce1657a3bc5dd9c82e
Putnjiek <annkathrin_rauch@icloud.com>
2025-10-22 09:51:34 +0200

    added taschenrechner files

---
+++ taschenrechner/taschenrechner.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <title>Taschenrechner</title>
+    <link rel="stylesheet" href="taschenrechner.css">
+</head>
+
+<body>
+    <h1>Taschenrechner</h1>
+</body>
+
+</html>
